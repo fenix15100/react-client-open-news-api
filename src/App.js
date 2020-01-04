956094bdc1e3c39dd5354c9520f635497e362128
fenix15100 <francisco.camacho15@gmail.com>
2020-01-04 12:38:44 +0100

    Implement getNewsFromRemote() in App component.
    +fetch all articles from remote api ans save it in state
    +Control errors in Promise request

--- src/App.js
+++ src/App.js
@@ -5,12 +5,48 @@ import Footer from './components/Footer'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+
+    this.state = {
+      news:[],
+      error:false
+     };
   }
+
+  componentDidMount(){
+    const requestPromise = this.getNewsFromRemote();
+    
+    requestPromise.then(data=>{
+      if(data.status!=='ok') throw new Error(`${data.code} ${data.message}`);
+
+      this.setState({
+        news:[...data.articles]
+      })
+
+    })
+    .catch(error=>{
+      console.error(`Request failed: ${error}`)
+      this.setState({
+        error:true
+      })
+    })
+  }
+
+  getNewsFromRemote = async () =>{
+
+    const endpoint = `https://newsapi.org/v2/top-headlines?country=us&category=business&apKey=${process.env.REACT_APP_TOKEN_NEWS_API}`;
+    let response = await fetch(endpoint);
+    let data = await response.json();
+    return data 
+    
+  }
+
+
+
   render() {
     return (
       <Fragment>
-        <h1>Its, works</h1>
+        <h1>Cliente de Noticias</h1>
+        
         <Footer/>
       </Fragment>  
     );
